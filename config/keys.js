09a15b44dc0e0c410319a6a9045382ac982c5bd5
Sadman255 <sadmanowar@gmail.com>
2022-02-16 21:58:23 -0500

    added create and search category

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://admin:oOZ6Hjst4E3BJFdk@cluster0.gewl3.mongodb.net/IPAT?authSource=admin&replicaSet=atlas-cpbil9-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true",
+};
---
+++ validation/categoryCreate.js
@@ -0,0 +1,39 @@
+const Validator = require("validator");
+const validText = require("./valid-text");
+
+module.exports = function validateCategoryCreateInput(data) {
+  let errors = { };
+
+  data.category = validText(data.category) ? data.category : "";
+  data.lob = validText(data.lob) ? data.lob : "";
+  data.product = validText(data.product) ? data.product : "";
+  data.type = validText(data.type) ? data.type : "";
+
+  if (!Validator.isLength(data.category, { min: 3, max: 140 })) {
+    errors.text = "Category must be between 3 and 140 characters ";
+  }
+
+  if (Validator.isEmpty(data.category)) {
+    errors.text = " Category field is required ";
+  }
+
+  if (
+    data.category.toLowerCase() == "lob" &&
+    Validator.isEmpty(data.lob)
+  ) {
+    errors.text = "A value for LOB is required if Category is LOB ";
+  }
+  if (data.category.toLowerCase() == "product" && Validator.isEmpty(data.product)) {
+    errors.text = "A value for PRODUCT is required if Category is PRODUCT ";
+  }
+
+  if (data.category.toLowerCase() == "lob" && Validator.isEmpty(data.lob)) {
+    errors.text = " Lob field is required if the category is lob ";
+  }
+
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0,
+  };
+};
