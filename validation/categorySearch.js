ec9d061d269ce95d832a58dce645602b651c821e
Sadman255 <sadmanowar@gmail.com>
2022-03-07 17:42:44 -0500

    added product,lob validation when category is common

--- validation/categorySearch.js
+++ validation/categorySearch.js
@@ -36,11 +36,11 @@ module.exports = function validateCategorySearchInput(data) {
 
   if (data.category.toLowerCase() == "common" && data.product) {
     errors.text = "Product should not exist if category is common";
-    console.log("entered common validation if product provided");
+    //console.log("entered common validation if product provided");
   }
   if (data.category.toLowerCase() == "common" && data.lob) {
     errors.text = "Lob should not exist if category is common";
-    console.log("enter common validation if lob provided");
+    //console.log("enter common validation if lob provided");
   }
 
   return {
